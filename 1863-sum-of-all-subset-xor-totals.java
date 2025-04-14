cc2ad3890970b3ff2f636f650780d082a5b85499
Sam Lu <pbcmarsh@gmail.com>
2025-04-14 02:32:11 -0500

    Update 1863-sum-of-all-subset-xor-totals.java

--- 1863-sum-of-all-subset-xor-totals.java
+++ 1863-sum-of-all-subset-xor-totals.java
@@ -1,5 +1,8 @@
 class Solution {
     public int subsetXORSum(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
         int total = 0;
         for (int num : nums) {
             total |= num;
