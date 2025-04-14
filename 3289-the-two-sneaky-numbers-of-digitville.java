1779b5d0f4a16b12be083266506deead8a2827e7
Sam Lu <pbcmarsh@gmail.com>
2025-04-14 14:44:16 -0500

    Create 3289-the-two-sneaky-numbers-of-digitville.java

---
+++ 3289-the-two-sneaky-numbers-of-digitville.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] getSneakyNumbers(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            throw new IllegalArgumentException("invalid");
+        }
+        int[] ans = new int[2];
+        int index = 0;
+        HashSet<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            if (!set.add(num)) {
+                if (index >= 2) {
+                    throw new IllegalStateException("invalid");
+                }
+                ans[index++] = num;
+            }
+        }
+        if (index < 2) {
+            throw new IllegalStateException("invalid");
+        }
+        return ans;
+    }
+}
