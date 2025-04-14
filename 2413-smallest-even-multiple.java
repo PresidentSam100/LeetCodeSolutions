4755125abae2057670b5e0597302447ca71efc89
Sam Lu <pbcmarsh@gmail.com>
2025-04-14 14:22:35 -0500

    Update 2413-smallest-even-multiple.java

--- 2413-smallest-even-multiple.java
+++ 2413-smallest-even-multiple.java
@@ -1,5 +1,8 @@
 class Solution {
     public int smallestEvenMultiple(int n) {
+        if (n <= 0) {
+            throw new IllegalArgumentException("invalid");
+        }
         return (n & 1) == 0 ? n : n << 1;
     }
 }
