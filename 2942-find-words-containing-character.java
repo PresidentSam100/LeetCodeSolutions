94189aebfec97d4001aeb3530295e686b5bc40b8
Sam Lu <pbcmarsh@gmail.com>
2025-04-14 15:14:32 -0500

    Update 2942-find-words-containing-character.java

--- 2942-find-words-containing-character.java
+++ 2942-find-words-containing-character.java
@@ -1,10 +1,13 @@
 class Solution {
     public List<Integer> findWordsContaining(String[] words, char x) {
+        if (words == null) {
+            throw new IllegalArgumentException("invalid");
+        }
         List<Integer> ans = new ArrayList<>();
         for (int i = 0; i < words.length; i++) {
             if (words[i].indexOf(x) != -1) {
                 ans.add(i);
-            }            
+            }
         }
         return ans;
     }
