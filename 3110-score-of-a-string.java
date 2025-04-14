223afb5e89d67aba549c4505147da8616b010b6c
Sam Lu <pbcmarsh@gmail.com>
2025-04-14 02:04:38 -0500

    Update 3110-score-of-a-string.java

--- 3110-score-of-a-string.java
+++ 3110-score-of-a-string.java
@@ -1,5 +1,8 @@
 class Solution {
     public int scoreOfString(String s) {
+        if (s == null || s.length() < 2) {
+            return 0;
+        }
         int score = 0;
         for (int i = 1; i < s.length(); i++) {
             score += Math.abs(s.charAt(i) - s.charAt(i - 1));
