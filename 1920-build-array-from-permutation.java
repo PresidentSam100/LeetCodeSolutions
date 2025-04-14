359c1ffc407eef0ff8352ebadb12fb319c82c025
Sam Lu <pbcmarsh@gmail.com>
2025-04-14 02:47:25 -0500

    Update 1920-build-array-from-permutation.java

--- 1920-build-array-from-permutation.java
+++ 1920-build-array-from-permutation.java
@@ -1,5 +1,8 @@
 class Solution {
     public int[] buildArray(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            throw new IllegalArgumentException("invalid");
+        }
         int n = nums.length;
         int[] ans = new int[n];
         for (int i = 0; i < n; i++) {
