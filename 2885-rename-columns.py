937ff2df4fe16c660562834c1a8b55e641338dbc
Sam Lu <pbcmarsh@gmail.com>
2025-04-16 13:36:43 -0500

    Update 2885-rename-columns.py

--- 2885-rename-columns.py
+++ 2885-rename-columns.py
@@ -1,4 +1,6 @@
 import pandas as pd
 
 def renameColumns(students: pd.DataFrame) -> pd.DataFrame:
+    if not isinstance(students, pd.DataFrame):
+        raise TypeError("invalid")
     return students.rename(columns={'id': 'student_id', 'first': 'first_name', 'last': 'last_name', 'age': 'age_in_years'})
