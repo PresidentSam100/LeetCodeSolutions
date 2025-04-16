d10d29a76a70462fd95d3440fc7a4bdc9b65e5c4
Sam Lu <pbcmarsh@gmail.com>
2025-04-16 12:38:49 -0500

    Update 1480-running-sum-of-1d-array.java

--- 1480-running-sum-of-1d-array.java
+++ 1480-running-sum-of-1d-array.java
@@ -3,13 +3,9 @@ class Solution {
         if (nums == null) {
             throw new IllegalArgumentException("invalid");
         }
-        int n = nums.length;
-        int[] ans = new int[n];
-        int sum = 0;
-        for (int i = 0; i < n; i++) {
-            sum += nums[i];
-            ans[i] = sum;
+        for (int i = 1; i < nums.length; i++) {
+            nums[i] += nums[i - 1];
         }
-        return ans;
+        return nums;
     }
 }
