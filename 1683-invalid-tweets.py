8e59404eaf3cf120daad998af75fb377894312fd
Sam Lu <pbcmarsh@gmail.com>
2025-04-16 13:40:17 -0500

    Update 1683-invalid-tweets.py

--- 1683-invalid-tweets.py
+++ 1683-invalid-tweets.py
@@ -1,4 +1,6 @@
 import pandas as pd
 
 def invalid_tweets(tweets: pd.DataFrame) -> pd.DataFrame:
+    if not isinstance(tweets, pd.DataFrame):
+        raise TypeError("invalid")
     return tweets.loc[tweets['content'].str.len() > 15, ['tweet_id']]
