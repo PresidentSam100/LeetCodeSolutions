de3042eba57cf73e9d873f738718a226fdaa4a39
Sam Lu <pbcmarsh@gmail.com>
2025-04-16 13:34:17 -0500

    Update 2882-drop-duplicate-rows.py

--- 2882-drop-duplicate-rows.py
+++ 2882-drop-duplicate-rows.py
@@ -1,4 +1,6 @@
 import pandas as pd
 
 def dropDuplicateEmails(customers: pd.DataFrame) -> pd.DataFrame:
+    if not isinstance(customers, pd.DataFrame):
+        raise TypeError("invalid")
     return customers.drop_duplicates(subset='email', keep='first')
