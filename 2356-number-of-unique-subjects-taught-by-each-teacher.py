82d7dd7c31dde56c4d3e27ecd5a26059cf6f256b
Sam Lu <pbcmarsh@gmail.com>
2025-04-16 11:07:45 -0500

    Update 2356-number-of-unique-subjects-taught-by-each-teacher.py

--- 2356-number-of-unique-subjects-taught-by-each-teacher.py
+++ 2356-number-of-unique-subjects-taught-by-each-teacher.py
@@ -1,4 +1,4 @@
 import pandas as pd
 
 def count_unique_subjects(teacher: pd.DataFrame) -> pd.DataFrame:
-    return teacher[['teacher_id', 'subject_id']].drop_duplicates().groupby('teacher_id').size().reset_index(name='cnt')
+    return teacher.groupby('teacher_id')['subject_id'].nunique().reset_index(name='cnt')
