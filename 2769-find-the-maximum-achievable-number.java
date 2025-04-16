d6559047f0d2ab3b8a409d0ce0dbeaa244643022
Sam Lu <pbcmarsh@gmail.com>
2025-04-16 10:44:08 -0500

    Update 2769-find-the-maximum-achievable-number.java

--- 2769-find-the-maximum-achievable-number.java
+++ 2769-find-the-maximum-achievable-number.java
@@ -1,5 +1,8 @@
 class Solution {
     public int theMaximumAchievableX(int num, int t) {
+        if (t <= 0) {
+            throw new IllegalArgumentException("invalid");
+        }
         return num + t * 2;
     }
 }
