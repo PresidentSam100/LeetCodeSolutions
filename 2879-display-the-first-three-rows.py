d56290de2297acc85fc411a2187044dca4b257eb
Sam Lu <pbcmarsh@gmail.com>
2025-04-18 10:18:49 -0500

    Update 2879-display-the-first-three-rows.py

--- 2879-display-the-first-three-rows.py
+++ 2879-display-the-first-three-rows.py
@@ -1,4 +1,6 @@
 import pandas as pd
 
 def selectFirstRows(employees: pd.DataFrame) -> pd.DataFrame:
+    if not isinstance(employees, pd.DataFrame):
+        raise TypeError("invalid")
     return employees.head(3)
