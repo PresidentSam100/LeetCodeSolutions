4ce2c71d4c1727f34310e53a711d0812534f6f99
Sam Lu <pbcmarsh@gmail.com>
2025-04-18 10:21:33 -0500

    Update 2884-modify-columns.py

--- 2884-modify-columns.py
+++ 2884-modify-columns.py
@@ -1,5 +1,11 @@
 import pandas as pd
 
 def modifySalaryColumn(employees: pd.DataFrame) -> pd.DataFrame:
+    if not isinstance(employees, pd.DataFrame):
+        raise TypeError("invalid")
+    if 'salary' not in employees.columns:
+        raise KeyError("invalid")
+    if not pd.api.types.is_numeric_dtype(employees['salary']):
+        raise ValueError("invalid")
     employees['salary'] *= 2
     return employees
