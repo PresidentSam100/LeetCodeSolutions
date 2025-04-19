0320f47120833c0aedfcacd370f0180f378c8ae5
Sam Lu <pbcmarsh@gmail.com>
2025-04-19 15:56:26 -0500

    Update 2119-a-number-after-a-double-reversal.java

--- 2119-a-number-after-a-double-reversal.java
+++ 2119-a-number-after-a-double-reversal.java
@@ -1,5 +1,8 @@
 class Solution {
     public boolean isSameAfterReversals(int num) {
+        if (num < 0) {
+            throw new IllegalArgumentException("invalid");
+        }
         return num == 0 || num % 10 != 0;
     }
 }
