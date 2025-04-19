f27fa22c87a7ae1a40f88d882578e5a8667b5a99
Sam Lu <pbcmarsh@gmail.com>
2025-04-19 17:47:59 -0500

    Update 3232-find-if-digit-game-can-be-won.java

--- 3232-find-if-digit-game-can-be-won.java
+++ 3232-find-if-digit-game-can-be-won.java
@@ -1,17 +1,17 @@
 class Solution {
     public boolean canAliceWin(int[] nums) {
-        if (nums == null | nums.length == 0) {
+        if (nums == null || nums.length == 0) {
             throw new IllegalArgumentException("invalid");
         }
         int singleSum = 0;
         int doubleSum = 0;
         for (int num : nums) {
-            if (num < 1 || num > 99) {
+            if (num <= 0 || num >= 100) {
                 throw new IllegalArgumentException("invalid");
             }
             if (num <= 9) {
                 singleSum += num;
-            } else if (num >= 10) {
+            } else {
                 doubleSum += num;
             }
         }
