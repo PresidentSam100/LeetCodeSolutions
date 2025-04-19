273fd0089c406d665f8319fbb0ab8dccb33da8d4
Sam Lu <pbcmarsh@gmail.com>
2025-04-19 18:33:51 -0500

    Update 1967-number-of-strings-that-appear-as-substrings-in-word.java

--- 1967-number-of-strings-that-appear-as-substrings-in-word.java
+++ 1967-number-of-strings-that-appear-as-substrings-in-word.java
@@ -1,14 +1,14 @@
 class Solution {
     public int numOfStrings(String[] patterns, String word) {
-        if (patterns == null || patterns.length == 0 || word == null) {
+        if (patterns == null || patterns.length == 0 || word == null || word.isEmpty()) {
             throw new IllegalArgumentException("invalid");
         }
         int ans = 0;
         for (String pattern : patterns) {
-            if (pattern == null) {
+            if (pattern == null || pattern.isEmpty()) {
                 throw new IllegalArgumentException("invalid");
             }
-            if (word.indexOf(pattern) != -1) {
+            if (word.contains(pattern)) {
                 ans++;
             }
         }
