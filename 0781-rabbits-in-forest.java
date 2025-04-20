2b92fa837875f2b6504dee475fa46396f7f4d174
Sam Lu <pbcmarsh@gmail.com>
2025-04-20 17:46:57 -0500

    Update 0781-rabbits-in-forest.java

--- 0781-rabbits-in-forest.java
+++ 0781-rabbits-in-forest.java
@@ -4,18 +4,16 @@ class Solution {
             throw new IllegalArgumentException("invalid");
         }
         HashMap<Integer, Integer> map = new HashMap<>();
-        int ans = 0;
         for (int answer : answers) {
             if (answer < 0) {
                 throw new IllegalArgumentException("invalid");
             }
-            if (!map.containsKey(answer)) {
-                ans += answer + 1;
-            }
-            map.put(answer, map.getOrDefault(answer, answer + 1) - 1);
-            if (map.get(answer) == 0) {
-                map.remove(answer);
-            }
+            map.put(answer, map.getOrDefault(answer, 0) + 1);
+        }
+        int ans = 0;
+        for (int key : map.keySet()) {
+            int size = (key + 1);
+            ans += (map.get(key) + key) / size * size;
         }
         return ans;
     }
