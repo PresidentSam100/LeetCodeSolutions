760d70619c67159bfb7df1a65f74eeb1ca7b9600
Sam Lu <pbcmarsh@gmail.com>
2025-04-19 20:06:52 -0500

    Create 3248-snake-in-matrix.java

---
+++ 3248-snake-in-matrix.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int finalPositionOfSnake(int n, List<String> commands) {
+        if (commands == null || n <= 1) {
+            throw new IllegalArgumentException("invalid");
+        }
+        int ans = 0;
+        for (String command : commands) {
+            ans += switch (command) {
+                case "RIGHT" -> 1;
+                case "LEFT" -> -1;
+                case "UP" -> -n;
+                case "DOWN" -> n;
+                default -> throw new IllegalArgumentException("invalid");
+            };
+            if (ans < 0 || ans >= n * n) {
+                throw new IllegalArgumentException("invalid");
+            }
+        }
+        return ans;
+    }
+}
