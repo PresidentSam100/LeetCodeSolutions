7987737dd421128c517aa4fa255ebb9ced802dd3
Sam Lu <pbcmarsh@gmail.com>
2025-05-02 19:59:04 -0500

    Update 1007-minimum-domino-rotations-for-equal-row.java

--- 1007-minimum-domino-rotations-for-equal-row.java
+++ 1007-minimum-domino-rotations-for-equal-row.java
@@ -1,8 +1,11 @@
 class Solution {
     public int minDominoRotations(int[] tops, int[] bottoms) {
+        if (tops == null || tops.length == 0 || bottoms == null || bottoms.length == 0 || tops.length != bottoms.length) {
+            throw new IllegalArgumentException("invalid");
+        }
         int[] nums = new int[]{tops[0], bottoms[0]};
         int n = Math.max(tops.length, bottoms.length);
-        int ans = Integer.MAX_VALUE;
+        int ans = n;
         for (int num : nums) {
             int tt = 0;
             int bb = 0;
@@ -16,12 +19,13 @@ class Solution {
                 } else if (t == num && b == num) {
                     continue;
                 } else {
-                    tt = bb = Integer.MAX_VALUE;
+                    tt = bb = n;
                     break;
                 }
             }
             ans = Math.min(ans, Math.min(tt, bb));
+            
         }
-        return (ans == Integer.MAX_VALUE) ? -1 : ans;
+        return (ans == n) ? -1 : ans;
     }
 }
